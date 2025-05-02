5785dff78bc7ff1925fbe8fce49c9f66799eca4f
Galygious <61418882+Galygious@users.noreply.github.com>
2025-05-02 00:49:22 -0500

    Update workflowLib.user.js

--- workflowLib.user.js
+++ workflowLib.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         workflowLib
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-02
+// @version      2025-05-02-v2
 // @description  try to take over the world!
 // @author       You
 // @match        *://*/*
